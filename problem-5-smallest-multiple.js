a1248e7ac56d9777914a2d065996e34d2cc84669
alfonsodlopez <adlopez15@gmail.com>
2020-01-12 20:29:07 -0800

    Add solution to problem 5 of Project Euler: Smallest Multiple

--- problem-5-smallest-multiple.js
+++ problem-5-smallest-multiple.js
@@ -1,3 +1,21 @@
+/* 
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+
+smallestMult(5) should return 60.
+Passed
+smallestMult(7) should return 420.
+Passed
+smallestMult(10) should return 2520.
+Passed
+smallestMult(13) should return 360360.
+Passed
+smallestMult(20) should return 232792560.
+Passed
+*/
+
+
 function smallestMult(n) {
     //Get max possible solution
     let product = 1
